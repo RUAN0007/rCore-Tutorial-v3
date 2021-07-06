9fdcab6dd8f9620420bd1590424d82aa1d6ec88a
ruanpc <ruanpingcheng@gmail.com>
2021-07-06 11:22:44 +0800

    Solution

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,6 +1,25 @@
 const FD_STDOUT: usize = 1;
+const USER_STACK_SIZE : usize = 0x1000;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
+pub fn sys_write(fd: usize, buf: *const u8, len: usize, cur_user_sp: *const u8) -> isize {
+    let top : usize = (cur_user_sp  as usize + USER_STACK_SIZE - 1) & (!(USER_STACK_SIZE - 1));
+    let bottom : usize = top as usize - USER_STACK_SIZE;
+    let mut in_stack : bool = false;
+    if bottom < buf as usize && buf as usize <= top {
+        in_stack = true;
+    }
 
-pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+    let mut in_space : bool = false;
+    if APP_BASE_ADDRESS <= (buf as usize) && (buf as usize) < APP_BASE_ADDRESS + APP_SIZE_LIMIT {
+        in_space = true;
+    }
+
+    if !in_space && !in_stack {
+        return -1isize;
+    }
+
+
     match fd {
         FD_STDOUT => {
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
